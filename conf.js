7d12c808fcdee223aa6751b0ae273b93484c71c2
xinlang029 <93472314+xinlang029@users.noreply.github.com>
2021-12-08 23:01:17 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -2,7 +2,7 @@ jsproxy_config({
   // 当前配置的版本（记录在日志中，用于排查问题）
   // 每次修改配置，该值需要增加，否则不会生效。
   // 默认每隔 5 分钟自动下载配置，若想立即验证，可通过隐私模式访问。
-  ver: '112',
+  ver: '113',
 
   // 通过 CDN 加速常用网站的静态资源（实验中）
   static_boost: {
@@ -19,10 +19,16 @@ jsproxy_config({
         
       }
    },
-    'dtxhbv-hh': {
+    'dtxhtfbv-h6h': {
       label: '029-002百度-节点',
       lines: {
-        '002.029hh.workers.dev': 1,
+        '002.002.029hh.workers.dev': 1,
+      }
+    },
+    'dtxhbv-hh': {
+      label: '029-0001百度-节点',
+      lines: {
+        '00001.0001.029hh.workers.dev/': 1,
       }
     },
     'demo-sg': {
